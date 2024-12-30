a9c73cc619426891c562170654a915430d8ea662
yindiana <yndiana.contact@gmail.com>
2024-12-30 13:48:56 +0100

    création du projet manga_dl pour télécharger des manga via des urls

---
+++ python/manga_dl/main.py
@@ -0,0 +1,25 @@
+#/usr/bin/python3
+
+DOCUMENTATION = r"""
+module: 
+author: yindiana.fr
+short_description: 
+description:
+    - 
+
+prérequis
+    - python -m pip install os
+
+options
+    N/A
+"""
+
+EXAMPLE = r"""
+python main.py
+"""
+
+RETURN = r"""
+
+"""
+
+import os
